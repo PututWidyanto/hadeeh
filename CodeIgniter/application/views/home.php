a3f421e4b2abf5f962fa62266ac3e1e241eed748
PututWidyanto <pututmemet@gmail.com>
2017-03-03 11:14:30 +0700

    susah
    biyooh

---
+++ CodeIgniter/application/views/home.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Putut</a>
+						</div>
+				
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse navbar-ex1-collapse">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="<?php echo site_url()?>/home">Home</a></li>
+								<li><a href="<?php echo site_url()?>/about">About</a></li>
+								<li><a href="<?php echo site_url()?>/contact">Contact</a></li>
+							</ul>
+								</li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div>
+				</nav>
+				<div class="jumbotron">
+					<div class="container">
+						<h1>Hello, Guys!</h1>
+						<p>Welcome To My Blog</p>
+						<p><center>Putut Widyanto</center></p>
+						
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<h1>Hallo guys! Di dalam blog sini ane mau kasih sedikit inspirasi dari biografi Albert Einstein, Checkidot!!!</h1>
+					Albert Einstein adalah seorang ilmuwan fisika teoretis yang dipandang luas sebagai ilmuwan terbesar dan sangat terkenal dalam abad ke-20. Beliau mengemukakan teori relativitas dan juga banyak menyumbang bagi pengembangan mekanika kuantum, mekanika statistika, dan kosmologi. 
+					<br>
+					<br>
+					Dia dianugerahi Penghargaan Nobel dalam Fisika atas pengabdiannya terhadap ilmu Fisika teoritis yang mengembangkan teori umum relativity pada tahun 1921. Salah satu rumus yang paling terkenal dan membuatnya banyak dikenal orang adalah rumus Teori Relativitas yaitu E=mc².
+					<br>
+					<br>
+					<br>
+					Biodata Albert Einstein
+					<br>
+					1. Nama Lengkap : Albert Einstein
+					<br>
+					2. Tanggal Lahir : 14 Maret 1879
+					<br>
+					3. Tempat Lahir : Ulm, Wuttemberg, Jerman
+					<br>
+					<br>
+					Kehidupan Keluarga dan Masa Kecil
+					<br>
+					Albert Einstein lahir di Ulm, Kerajaan Wuttemberg, Jerman pada tanggal 14 Maret 1879. Ayah Albert Einstein bernama Hermann Einstein dan Ibunya bernama Pauline mereka berdua menikah di Stuttgart-Bad Cannstatt. Keluarga Einstein termasuk keluarga keturunan Yahudi. Ayahnya hanya bekerja sebagai penjaja ranjang bulu yang kemudian beralih pekerjaan menjadi ahli elektrokimia. Keluarga yang dimiliki oleh Einstein sangatlah konsen terhadap 
+					pendidikan anaknya terutama dibidang sains dan musik.
+					<br>
+					<br>
+					Keluarga Einstein menyekolahkannya di sekolah Katholik dan atas keinginan dari ibunya dia diberi pelajaran bermain musik karena keluarga dari Einstein suka dengan musik. Saat berusia 5 tahun, ayahnya menunjukkan sebuah benda kompas kantung kepada Einstein, dan Einstein melihat bahwa sesuatu di ruang yang "kosong" ini bereaksi terhadap jarum di dalam kompas tersebut, dia menjelaskan pengalamannya ini sebagai salah satu saat yang paling menyenangkan dalam hidupnya, dan dari situlah perkenalan Albert Einstein terhadap sains terus ditingkatkan. 
+					<br>
+					<br>
+					Albert Einstein memiliki sifat pendiam dan tidak suka bermain dengan teman-temannya. Selain bersekolah dan menggeluti sains, kegiatan Einstein hanyalah bermain musik bersama dengan ibunya memainkan karya-karya Mozart dan Bethoveen.
+					<br>
+					<br>
+					Albert Einstein dianggap sebagai pelajar yang lambat, kemungkinan disebabkan oleh dyslexia, sifat pemalu, atau karena struktur yang jarang dan tidak biasa pada otaknya (diteliti setelah kematiannya). Dia kemudian diberikan penghargaan untuk teori relativitasnya karena kelambatannya ini, dan berkata dengan berpikir dalam tentang ruang dan waktu dari anak-anak lainnya, dia mampu mengembangkan kepandaian yang lebih berkembang. Pendapat lainnya, berkembang belakangan ini, tentang perkembangan mentalnya adalah dia menderita Sindrom Asperger, sebuah kondisi yang berhubungan dengan autisme.
+					<br>
+					<br>
+					Albert Einstein mengalami kesulitan saat mengikuti mata pelajaran di sekolahnya terutama dalam bidang berhitung dan ilmu alam. Albert Einstein dianggap murid yang terbelakang di sekolahnya. Hal ini dikarenakan kepribadiannya yang introvet dan pemalu, namun setelah diteliti otaknya saat ia meninggal dunia, hal ini dikarenakan struktur otaknya yang tidak biasa dari umumnya orang. Einstein memiliki kecenderungan berfikir dengan olah pikirnya sendiri sedangkan pelajaran sekolah menyuruh murid untuk berfikir dengan cara orang lain yaitu gurunya, hal inilah yang membuat Einstein kesulitan mengikuti alur belajar di sekolah.
+					<br>
+					<br>
+					Dua pamannya pada saat itu ikut aktif membantu mengembangkan ketertarikannya terhadap dunia intelek pada masa akhir kanak-kanaknya dan awal remaja dengan memberikan usulan dan buku tentang sains dan matematika. Saat Einstein berusia 15 tahun, yaitu tahun 1984, keadaan ekonomi keluarganya mengalami kemunduran karena usaha ayahnya dibidang elektronika mengalami kebangkrutan. Hal tersebut membuat Einstein dan keluarganya pindah ke Pavia, Milan-Italia. Einstein tetap tinggal untuk menyelesaikan sekolahnya, dan menyelesaikan satu semester sebelum bergabung kembali dengan keluarganya di Pavia. 
+					<br>
+					<br>
+					Albert Einstein masuk ke Eidgenössische Technische Hochschule (Institut Teknologi Swiss Federal, di Zurich). namun ia gagal saat tes. Pada tahun berikutnya dia dikirim oleh keluarganya ke Aarau, Swiss, untuk menyelesaikan sekolah menengahnya, di mana dia menerima diploma pada tahun 1896, Einstein tercatat beberapa kali mendaftar di Eidgenössische Technische Hochschule.
+					<br>
+					<br>
+					Pada tahun 1896 saat Einstein berumur 17 tahun, ia memutuskan untuk melepas kewarganegaraan dan memilih untuk tidak punya warga negara karena kecewa dengan pemerintahan Jerman yang anti Yahudi pada saat itu. Ini juga didukung bahwa sepeninggal Einstein dari Jerman, Nazi yang terkenal anti Yahudi menguasai Jerman dan memusuhi orang Yahudi.
+				</div>
+			</div>	
+		</div>
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
